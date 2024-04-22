665acead6dde375648603630cccade1c74f9b3ee
Jakaria Hossain <mdjak8980@gmail.com>
2024-04-22 13:07:05 +0600

    22/4/2024

--- src/Pages/Details/CarDetails/CarDetails.jsx
+++ src/Pages/Details/CarDetails/CarDetails.jsx
@@ -14,19 +14,19 @@ const CarDetails = () => {
             <figure className='bg-indigo-300'><img className='w-2/3 animate-pulse' src={`https://auto-car-world-server-jaki-pro-jakaria-hossains-projects.vercel.app/${img}`} alt="Shoes" /></figure>
             <div className="card-body bg-indigo-200">
                 <h2 className="">
-                    <div className='flex justify-between'>
-                        <p className='text-3xl'>{company}</p>
-                        <p className='font-semibold'>Color: {color}</p>
+                    <div className='flex justify-between items-center'>
+                        <p className='text-3xl mr-2'>{company}</p>
+                        <p className='font-semibold text-xs sm:text-lg text-[#009dff]'>Color: {color}</p>
                     </div>
                     <div>
                         {model}
                         <div className="badge badge-secondary"> NEW</div>
                     </div>
                 </h2>
-                <p>{description}</p>
+                <p className='text-justify'>{description}</p>
                 <div className='flex justify-between'>
-                    <div>
-                        <h1 className='text-2xl font-semibold'>New Updates</h1>
+                    <div className='mr-2'>
+                        <h1 className='text-md sm:text-2xl font-semibold'>New Updates</h1>
                         <hr />
                         <ul>
                             {
@@ -37,22 +37,21 @@ const CarDetails = () => {
                         </ul>
                     </div>
                     <div>
-                        <h1 className='text-2xl font-semibold'>About</h1>
+                        <h1 className='text-md sm:text-2xl font-semibold'>About</h1>
                         <hr />
                         <h4>Year: {year}</h4>
                         <h4>Engine: {engine}</h4>
                         <h4>Fuel Type: {fuel_type}</h4>
-                        <h4>Transmission: {transmission}</h4>
+                        <p className='break-words'>Transmission: {transmission}</p>
                     </div>
                 </div>
                 <hr />
-                <div className=' flex justify-between items-end'>
+                <p className='text-[green] font-bold'>Price: {price} $</p> 
+                <div className=' flex justify-between gap-2 items-end'>
                     <div>
-
-                        <p className='text-[green] font-bold'>Price: {price} $</p> 
-                        <Link to={`/order-request/${_id}`}><button className='btn btn-primary'>Order Now</button></Link>
+                        <Link to={`/order-request/${_id}`}><button className='btn btn-primary text-xs sm:text-lg'>Order Now</button></Link>
                     </div>
-                    <Link onClick={()=>!user&&alert('you should login first')} to={user?.email?`/rent-car/${_id}`:'/login'}><button className='btn btn-primary'>Apply for Rent</button></Link>
+                    <Link onClick={()=>!user&&alert('you should login first')} to={user?.email?`/rent-car/${_id}`:'/login'}><button className='btn btn-primary text-xs sm:text-lg'>Apply for Rent</button></Link>
                 </div>
             </div>
         </div>
